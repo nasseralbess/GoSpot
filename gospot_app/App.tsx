655a04638d248243d316afd171ef1dfa5bc320fa
Al-Jazzazi <ya2225@nyu.edu>
2024-07-12 00:26:25 -0400

    new animation changes, card swipe, still not working

--- gospot_app/App.tsx
+++ gospot_app/App.tsx
@@ -1,63 +1,83 @@
 import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
-import { Pressable, Text, StyleSheet, View, useWindowDimensions } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, useWindowDimensions } from 'react-native';
 import Card from "./src/components/TinderCard/Index";
 import users from "./TinderAssets/data/users";
-import Animated, { useSharedValue, useAnimatedStyle, withSpring, interpolate, useDerivedValue } from 'react-native-reanimated';
-import { GestureDetector, GestureHandlerRootView, PanGestureHandler, Gesture } from 'react-native-gesture-handler';
+import Animated, { useSharedValue, useAnimatedStyle, withSpring, interpolate, useDerivedValue, runOnJS } from 'react-native-reanimated';
+import { GestureDetector, GestureHandlerRootView, Gesture } from 'react-native-gesture-handler';
 
-
-const jeff = {
-  name: "Jeff",
-  bio: "Im Jeff",
-  image: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png",
-
-}
+const ROTATION = 60;
+const SWIPE_VELOCITY = 800;
 
 export default function App() {
-  const {width: screenWidth} = useWindowDimensions();
-  const translateX = useSharedValue(0);
-  const rotate= useDerivedValue(() => interpolate( translateX.value, [0, screenWidth],  [0,60]) + 'deg');
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [nextIndex, setNextIndex] = useState(currentIndex + 1);
 
-  const cardSyle = useAnimatedStyle(() => ({
+  const currentUser = users[currentIndex % users.length];
+  const nextUser = users[nextIndex % users.length];
+
+  const { width: screenWidth } = useWindowDimensions();
+  const translateX = useSharedValue(0);
+  const hiddenTranslateX = 2 * screenWidth;
+
+  const rotate = useDerivedValue(() =>
+    interpolate(translateX.value, [0, hiddenTranslateX], [0, ROTATION]) + 'deg'
+  );
+
+  const cardStyle = useAnimatedStyle(() => ({
     transform: [
       {
         translateX: translateX.value,
-
       },
       {
         rotate: rotate.value,
-      }
+      },
     ],
-    // opacity : sharedValue.value,
   }));
-  const handlePress = () => {
-    translateX.value = withSpring(Math.random()); // Example action
-  };
+
+  const nextCardStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        scale: interpolate(translateX.value, [-hiddenTranslateX, 0, hiddenTranslateX], [1, 0.8, 1]),
+      },
+    ],
+    opacity: interpolate(translateX.value, [-hiddenTranslateX, 0, hiddenTranslateX], [1, 0.6, 1]),
+  }));
 
   const panGesture = Gesture.Pan()
-    .onStart(() => {
-      console.log("started");
-    })
     .onUpdate((event) => {
       translateX.value = event.translationX;
     })
-    .onEnd(() => {
-      translateX.value = withSpring(0);
+    .onEnd((event) => {
+      if (Math.abs(event.velocityX) < SWIPE_VELOCITY) {
+        translateX.value = withSpring(0);
+        return;
+      }
+
+      translateX.value = withSpring(
+        hiddenTranslateX * Math.sign(event.velocityX),
+        {},
+        () => runOnJS(setCurrentIndex)(currentIndex + 1)
+      );
     });
 
+  useEffect(() => {
+    translateX.value = 0;
+    setNextIndex(currentIndex + 1);
+  }, [currentIndex, translateX]);
 
   return (
     <GestureHandlerRootView style={styles.pageContainer}>
+      <View style={styles.nextCardContainer}>
+        <Animated.View style={[styles.animatedCards, nextCardStyle]}>
+          <Card user={nextUser} />
+        </Animated.View>
+      </View>
       <GestureDetector gesture={panGesture}>
-        <Animated.View style={[styles.AnimatedCards, cardSyle]}>
-          <Card user={users[2]} />
+        <Animated.View style={[styles.animatedCards, cardStyle]}>
+          <Card user={currentUser} />
         </Animated.View>
       </GestureDetector>
-      <Pressable onPress={handlePress}>
-        <Text>Change Value</Text>
-      </Pressable>
-
     </GestureHandlerRootView>
   );
 }
@@ -68,17 +88,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  AnimatedCards: {
-    width: "100%",
+  animatedCards: {
+    width: '90%',
+    height: '70%',
+    position: 'absolute',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  nextCardContainer: {
+    ...StyleSheet.absoluteFillObject,
+    alignItems: 'center',
+    justifyContent: 'center', 
+  },
 });
-
-
-
-
-
-
-
-
