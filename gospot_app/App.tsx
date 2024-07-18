94adec701e752000ea81dce4c36b49cc7ef8adae
Al-Jazzazi <ya2225@nyu.edu>
2024-07-17 20:17:01 -0400

    switching to deck-swiper package

--- gospot_app/App.tsx
+++ gospot_app/App.tsx
@@ -1,82 +1,92 @@
 import 'react-native-gesture-handler';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, useWindowDimensions } from 'react-native';
-import Card from "./src/components/TinderCard/Index";
+import { StyleSheet, View, useWindowDimensions, Text, ImageBackground, Image } from 'react-native';
+//import Card from "./src/components/TinderCard/Index";
 import users from "./TinderAssets/data/users";
-import Animated, { useSharedValue, useAnimatedStyle, withSpring, interpolate, runOnJS} from 'react-native-reanimated';
-import { GestureDetector, GestureHandlerRootView, Gesture } from 'react-native-gesture-handler';
+import Animated, { useSharedValue, useAnimatedStyle, withSpring, interpolate, runOnJS } from 'react-native-reanimated';
+// import { GestureDetector, GestureHandlerRootView, Gesture } from 'react-native-gesture-handler';
+import Swiper from 'react-native-deck-swiper';
+import Like from "./TinderAssets/images/LIKE.png";
+import nope from "./TinderAssets/images/nope.png";
 
 const ROTATION = 60;
 const SWIPE_VELOCITY = 800;
 
 export default function App() {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [showLike, setShowLike] = useState(false);
+  const [showNope, setShowNope] = useState(false);
 
-  const currentUser = users[currentIndex % users.length];
-  const nextUser = users[(currentIndex + 1) % users.length];
+  const [index, setIndex] = useState(0);
+  const handleSwipedLeft = (cardIndex) => {
+    console.log('Swiped left:', cardIndex);
+    setShowNope(true);
+    setTimeout(() => {
+      setShowNope(false);
+    }, 500); // Show the like image for 1 second
+  };
 
-  const { width: screenWidth } = useWindowDimensions();
-  const translateX = useSharedValue(0);
-  const hiddenTranslateX = 2 * screenWidth;
+  const handleSwipedRight = (cardIndex) => {
+    console.log('Swiped right:', cardIndex);
+    setShowLike(true);
+    setTimeout(() => {
+      setShowLike(false);
+    }, 500); // Show the like image for 1 second
+  };
 
-  const cardStyle = useAnimatedStyle(() => ({
-    transform: [
-      {
-        translateX: translateX.value,
-      },
-      {
-        rotate: interpolate(translateX.value, [0, hiddenTranslateX], [0, ROTATION]) + 'deg',
-      },
-    ],
-  }));
-
-  const nextCardStyle = useAnimatedStyle(() => ({
-    transform: [
-      {
-        scale: interpolate(translateX.value, [-hiddenTranslateX, 0, hiddenTranslateX], [1, 0.8, 1]),
-      },
-    ],
-    opacity: interpolate(translateX.value, [-hiddenTranslateX, 0, hiddenTranslateX], [1, 0.6, 1]),
-  }));
-
-  const panGesture = Gesture.Pan()
-    .onUpdate((event) => {
-      translateX.value = event.translationX;
-    })
-    .onEnd((event) => {
-      if (Math.abs(event.velocityX) < SWIPE_VELOCITY) {
-        translateX.value = withSpring(0);
-        return;
-      }
-
-      translateX.value = withSpring(
-        hiddenTranslateX * Math.sign(event.velocityX),
-        {},
-        () => {
-          runOnJS(setCurrentIndex)((currentIndex + 1) % users.length);
-          translateX.value = 0; // Reset translateX after swipe
-        }
-      );
-    });
-
-  useEffect(() => {
-    translateX.value = 0;
-  }, [currentIndex]);
+  
 
   return (
-    <GestureHandlerRootView style={styles.pageContainer}>
-      {nextUser &&<View style={styles.nextCardContainer}>
-        <Animated.View style={[styles.animatedCards, nextCardStyle]}>
-          <Card user={nextUser} />
-        </Animated.View>
-      </View>} 
-      
-      {currentUser && <GestureDetector gesture={panGesture}>
-        <Animated.View style={[styles.animatedCards, cardStyle]}>
-          <Card user={currentUser} />
-        </Animated.View>
-      </GestureDetector>} 
-    </GestureHandlerRootView>
+    <View style={styles.pageContainer}>
+   
+      <Swiper
+        cards={users}
+        renderCard={(user) => ( 
+          <View style={styles.card}>
+            <ImageBackground source={{ uri: user.image }} style={styles.image}  >
+              <View style={styles.cardInner}>
+                <Text style={styles.name}> {user.name} </Text>
+                <Text style={styles.bio} >{user.bio} </Text>
+              </View>
+            </ImageBackground>
+            
+
+          </View>
+          
+        )}
+         onSwipedLeft={(cardIndex) => {
+          handleSwipedLeft(cardIndex);
+          setIndex(cardIndex + 1);
+        }}
+        onSwipedRight={(cardIndex) => {
+          handleSwipedRight(cardIndex);
+          setIndex(cardIndex + 1);
+        }}
+        onSwipedAll={() => {
+          console.log('All cards swiped');
+          setIndex(0); // Reset the index to loop the cards
+        }}
+        cardIndex={index}
+        backgroundColor={'#fffff'}
+        stackSize={3}
+        animateCardOpacity ={true}
+        verticalSwipe={false}
+
+      />
+      {showLike && (
+        <Animated.Image 
+          source={Like} 
+          style={[styles.like]} 
+          resizeMode="contain" 
+        />
+      )}
+       {showNope && (
+          <Animated.Image 
+            source={nope} 
+            style={[styles.like]} 
+            resizeMode="contain" 
+          />
+      )}
+    </View>
   );
 }
 
@@ -85,17 +95,66 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+
+
   },
-  animatedCards: {
-    width: '90%',
-    height: '70%',
-    position: 'absolute',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
   nextCardContainer: {
     ...StyleSheet.absoluteFillObject,
     alignItems: 'center',
     justifyContent: 'center',
+
+  },
+  image:
+  {
+    width: "100%",
+    height: "100%",
+    borderRadius: 10,
+    overflow: "hidden",
+    justifyContent: "flex-end",
+
+
+  },
+  card: {
+    flex: 1,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: "#E8E8E8",
+    justifyContent: "center",
+    backgroundColor: "white",
+    shadowColor: "#000000",
+    shadowOffset: {
+      width: 0,
+      height: 11,
+    },
+    shadowOpacity: 0.36,
+    shadowRadius: 11.78,
+    elevation: 15,
+  },
+  cardInner: {
+    padding: 10,
+
+  },
+  name: {
+    fontSize: 30,
+    color: "white",
+    fontWeight: "bold",
+    marginHorizontal: 10,
+
+
+  },
+  bio: {
+    fontSize: 18,
+    color: "white",
+    lineHeight: 25,
+  },
+  like: {
+    width: 100,
+    height: 100,
+    position: "absolute",
+    top: 50,
+    left: '50%',
+    transform: [{ translateX: -50 }],
+    zIndex: 1000, // Ensure the like image appears above other elements
   },
 });
