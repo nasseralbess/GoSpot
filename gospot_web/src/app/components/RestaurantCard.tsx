508155d98c189e1a39f7f1afa06af33c9ecfdb7e
Mohammad Labadi <allabadimohammad@outlook.com>
2024-08-24 14:33:04 -0400

    fix details and restaurant card

--- gospot_web/src/app/components/RestaurantCard.tsx
+++ gospot_web/src/app/components/RestaurantCard.tsx
@@ -1,10 +1,21 @@
 // src/app/components/RestaurantCard.tsx
+import React from 'react';
 import styles from '../styles/RestaurantCard.module.css';
 
 const RestaurantCard = ({ restaurant }) => {
+  const handleClose = () => {
+    // Logic to handle card close (e.g., setting state to hide the card)
+    console.log('Close card');
+  };
+
   return (
     <li className={styles.card}>
-      <img src={restaurant.image_url} alt={restaurant.name} />
+      <div className={styles.closeButton} onClick={handleClose}>
+        &times; {/* Unicode for "X" mark */}
+      </div>
+      <div className={styles.cardImageWrapper}>
+        <img src={restaurant.image_url} alt={restaurant.name} className={styles.cardImage} />
+      </div>
       <div className={styles.cardContent}>
         <h2 className={styles.cardTitle}>{restaurant.name}</h2>
         <p className={styles.cardDescription}>
@@ -13,8 +24,11 @@ const RestaurantCard = ({ restaurant }) => {
         <p>Phone: {restaurant.display_phone}</p>
         <p>Distance: {restaurant.distance.toFixed(2)} meters</p>
         <p>Transactions: {restaurant.transactions.join(', ')}</p>
-        <a href={restaurant.url} target="_blank" rel="noopener noreferrer">
+        <a href={restaurant.url} target="_blank" rel="noopener noreferrer" className={styles.ctaButton}>
           Visit Yelp Page
+        </a>
+        <a href={`/restaurants/${restaurant.id}`} className={styles.moreDetailsButton}>
+          More Details
         </a>
       </div>
     </li>
